7bf3dd4fedee1ec817536a690df9de2cd2efa427
Ubuntu <ubuntu@ip-172-31-23-51.us-east-2.compute.internal>
2022-11-22 03:13:58 +0000

    Refactored code to convert observation to dataframe in post method of main.py -- get keys directly from observation object.

--- starter/main.py
+++ starter/main.py
@@ -1,7 +1,9 @@
 # Put the code for your API here.
 from fastapi import FastAPI
+import json
 import pandas as pd
 from pydantic import BaseModel
+from typing import List, Union
 
 
 app = FastAPI()
@@ -19,30 +21,28 @@ async def get_greeting():
 
 
 class Observation(BaseModel):
-    age: int
-    workclass: str
+    age: Union[int, List[int]]
+    workclass: Union[str, List[str]]
+    fnlgt: Union[int, List[int]]
+    education: Union[str, List[str]]
+    education_num: Union[int, List[int]]
 
 
-    # fnlgt: int
-    # education: str
-    # education-num: int
+    # marital_status: Union[str, List[str]]
+    # occupation: Union[str, List[str]]
+    # relationship: Union[str, List[str]]
+    # race: Union[str, List[str]]
+    # sex: Union[str, List[str]]
+    # capital_gain: Union[int, List[int]]
+    # capital_loss: Union[int, List[int]]
 
 
 
 @app.post("/scoring/")
 async def score_observations(observation: Observation):
-    num_obs = (
-        len(observation.age) if isinstance(observation.age, list)
-        else 1
-    )
-    df = pd.DataFrame(
-        {
-            "age" : observation.age,
-            "workclass" : observation.workclass
-        },
-        index=list(range(num_obs))
-    )
+    observation_dict = json.loads(observation.json())
+    df = pd.DataFrame(observation_dict)
     df.age = df.age + 1
-    return df.to_json()
+    return df.to_dict()
 
 
